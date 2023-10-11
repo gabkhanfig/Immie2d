8d155cfbd6b256a5457370c4831c0bdfc5b20684
gabkhanfig <gabkhanfig@gmail.com>
2023-10-10 22:12:25 -0400

    Type Uses Byte Array Instead of Bitmask

--- immie2d_shared/src/gameplay/types/type_data.rs
+++ immie2d_shared/src/gameplay/types/type_data.rs
@@ -1,40 +1,68 @@
-/* Used as bitshift */
-#[repr(u32)]
-pub enum TypeKind {
-    Standard = 0,
-    Fire = 1,
-    Water = 2,
-    Nature = 3,
-    Electric = 4,
-    Air = 5,
-    Ground = 6,
-    Metal = 7,
-    Light = 8,
-    Dark = 9,
-    Dragon = 10
-}
+use std::fmt::Display;
+use std::arch;
+
+use super::type_kinds::TypeKind;
+use super::type_kinds::TYPE_COUNT;
 
 /* Type is a bitmask of multiple TypeFlags. */
-pub struct Type {
-    bitmask: u32
+pub struct Type { 
+    type_count: u8,
+    types: [TypeKind; TYPE_COUNT as usize] // consumes less memory than vec.
 }
 
 impl Type {
-
     pub fn new(in_types: Vec<TypeKind>) -> Type {
         assert!(in_types.len() > 0, "Cannot create an instance of Type with 0 types. See enum TypeKind");
-        let mut t = Type {
-            bitmask: 0,
+        let mut type_data = Type {
+            type_count: 0,
+            types: [TypeKind::Invalid; TYPE_COUNT as usize]
         };
-        for in_type in in_types {
-            t.bitmask |= 1 << in_type as u32;
+        for t in in_types {
+            type_data.add_type(t);
         }
-        return t;
+        return type_data;
     }
 
     pub fn has_type(&self, in_type: TypeKind) -> bool {
-        return self.bitmask & (1 << in_type as u32) != 0;
+        for i in 0..self.type_count as usize {
+            if self.types[i] == in_type  { return true; }
+        }
+        return false;
     }
+
+    pub fn add_type(&mut self, in_type: TypeKind) {
+        assert!(!self.has_type(in_type), "Type cannot contain duplicate types. Attempted to add type: {:?}\nThe current types are: {:?}", in_type, self.get_types());
+        self.types[self.type_count as usize] = in_type;
+        self.type_count += 1;
+    }
+
+    pub fn get_types(&self) -> Vec<TypeKind> {
+        let mut v: Vec<TypeKind> = Vec::new();
+        for t in self.iter() {
+            v.push(t);
+        }
+        return v;
+    }
+
+    pub fn iter(&self) -> TypeIter<'_> {
+        return TypeIter { types: &self, index: 0 };
+    }
+
 }
 
+pub struct TypeIter<'a> {
+    types: &'a Type,
+    index: usize
+}
 
+impl<'a> Iterator for TypeIter<'a> {
+    type Item = TypeKind;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.types.type_count as usize {
+            return None;
+        }
+        self.index += 1;
+        return Some(self.types.types[self.index - 1]);
+    }
+}
