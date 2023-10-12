37e8af3637981a93ec5e041640b0aa56906d82a7
gabkhanfig <gabkhanfig@gmail.com>
2023-10-12 01:56:53 -0400

    Ability Trait and Ability Dependency Injection

---
+++ immie2d_shared/src/gameplay/ability/ability.rs
@@ -0,0 +1,83 @@
+use std::collections::HashMap;
+
+use super::super::types::type_data::Type;
+
+
+pub trait Ability {
+    fn new() -> Box<dyn Ability>
+    where Self: Sized;
+
+    fn get_name(&self) -> &'static str;
+
+    fn static_name() -> &'static str
+    where Self: Sized;
+
+    fn get_base_ability_data(&self) -> &BaseAbilityData;
+    
+    fn get_base_ability_data_mut(&mut self) -> &mut BaseAbilityData;
+}
+
+pub enum AbilityCategory {
+    Attack,
+    Status
+}
+
+pub struct BaseAbilityData {
+    pub category: AbilityCategory,
+    pub types: Type,
+    pub power: f32,
+    pub speed: f32,
+}
+
+pub struct AbilityMap {
+    map: HashMap<&'static str, fn() -> Box<dyn Ability>>
+}
+
+impl AbilityMap {
+    pub fn new() -> Self {
+        return AbilityMap { map: HashMap::new() };
+    }
+
+    /// Dependency inject ability.
+    /// ```
+    /// use immie2d_shared::gameplay::ability::{ability::AbilityMap, abilities::fireball::Fireball};
+    /// let mut map = AbilityMap::new();
+    /// map.add_ability::<Fireball>();
+    /// ```
+    pub fn add_ability<T: Ability>(&mut self) {
+        self.map.insert(T::static_name(), T::new);
+    }
+
+    /// Create a new instance of Ability.
+    /// ```
+    /// # use immie2d_shared::gameplay::ability::{ability::AbilityMap, abilities::fireball::Fireball};
+    /// let mut map = AbilityMap::new();
+    /// map.add_ability::<Fireball>();
+    /// let ability = map.new_ability("fireball");
+    /// ```
+    /// Will panic if ability name doesn't exist. See AbilityMap::is_ability_name()
+    /// ``` should_panic
+    /// # use immie2d_shared::gameplay::ability::{ability::AbilityMap, abilities::fireball::Fireball};
+    /// # let mut map = AbilityMap::new();
+    /// # map.add_ability::<Fireball>();
+    /// // Will panic
+    /// let ability2 = map.new_ability("aksdaiuhsdpiauhsd");
+    /// ```
+    pub fn new_ability(&self, name: &str) -> Box<dyn Ability> {
+        let entry = self.map.get(name).expect(format!("Ability name [{}] is not valid", name).as_str());
+        return entry();
+    }
+
+    /// Check if an ability name is valid.
+    /// ```
+    /// # use immie2d_shared::gameplay::ability::{ability::AbilityMap, abilities::fireball::Fireball};
+    /// let mut map = AbilityMap::new();
+    /// map.add_ability::<Fireball>();
+    /// assert!(map.is_ability_name("fireball") == true);
+    /// assert!(map.is_ability_name("wuhafjnb") == false);
+    /// ```
+    pub fn is_ability_name(&self, name: &str) -> bool {
+        return self.map.contains_key(name);
+    }
+
+}
