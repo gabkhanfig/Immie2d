675921e057bc890135d5d09e0982c3a34a2e5e24
gabkhanfig <gabkhanfig@gmail.com>
2023-10-26 22:33:11 -0400

    Fixed Global String To Work With HashMap/Set Realloc

--- immie2d_shared/src/engine_types/global_string.rs
+++ immie2d_shared/src/engine_types/global_string.rs
@@ -1,21 +1,32 @@
-use std::{collections::HashSet, fmt};
+use std::{collections::{ HashSet, HashMap }, fmt};
 
 use lazy_static::lazy_static;
 use std::sync::Mutex;
 
+struct GlobalStringMaps {
+    map: HashMap<String, u32>,
+    vec: Vec<String>,
+    next_id: u32
+}
+
 lazy_static! {
-    static ref GLOBAL_STRING_SET: Mutex<HashSet<String>> = {
-        let m = HashSet::new();
-        Mutex::new(m)
+    static ref GLOBAL_STRING_MAP: Mutex<GlobalStringMaps> = {
+        let mut maps = GlobalStringMaps {
+            map: HashMap::new(),
+            vec: Vec::new(),
+            next_id: 0
+        };
+        maps.map.insert("".to_string(), 0);
+        maps.next_id = 1;
+        maps.vec.push("".to_string());
+        Mutex::new(maps)
     };
-    static ref EMPTY_STRING: String = { 
-        "".to_string() 
-    };
+
 }
 
 #[derive(Clone, Copy, PartialEq)]
 pub struct GlobalString {
-    internal_string: *const String
+    string_id: u32 
 }
 
 impl GlobalString {   
@@ -23,11 +34,11 @@ impl GlobalString {
     /// ```
     /// use immie2d_shared::engine_types::global_string::GlobalString;
     /// let gstr = GlobalString::default();
-    /// assert_eq!(gstr.to_string(), &"".to_string());
+    /// assert_eq!(gstr.to_string(), "".to_string());
     /// ```
     pub fn default() -> GlobalString {
         return GlobalString { 
-            internal_string: &*EMPTY_STRING as *const String
+            string_id: 0
         };
     }
 
@@ -36,15 +47,23 @@ impl GlobalString {
     /// ```
     /// use immie2d_shared::engine_types::global_string::GlobalString;
     /// let gstr = GlobalString::new(&"hello world!".to_string());
-    /// assert_eq!(gstr.to_string(), &"hello world!".to_string());
+    /// assert_eq!(gstr.to_string(), "hello world!".to_string());
     /// ```
     pub fn new(in_string: &String) -> GlobalString {
-        let mut map = GLOBAL_STRING_SET.lock().unwrap();
-        map.insert(in_string.clone());
-        let mapped_string = map.get(in_string).unwrap();
+        //println!("Adding GlobalString {}", in_string);
+        let mut maps = GLOBAL_STRING_MAP.lock().unwrap();
+        let next_id = maps.next_id;
+        let exists = maps.map.insert(in_string.clone(), next_id.clone());
+        if exists.is_some() { // If the value already exists in the map, just use the existing id
+            return GlobalString {
+                string_id: exists.unwrap()
+            };
+        }
+        maps.next_id += 1;
+        maps.vec.push(in_string.clone());
         return GlobalString {
-            internal_string: mapped_string as *const String
-        };      
+            string_id: next_id
+        };
     }
 
     /// Will create a new GlobalString instance containing a pointer to the argument string
@@ -52,42 +71,42 @@ impl GlobalString {
     /// ```
     /// use immie2d_shared::engine_types::global_string::GlobalString;
     /// let gstr = GlobalString::new_if_exists(&"some random string not in the map".to_string());
-    /// assert_eq!(gstr.to_string(), &"".to_string());
+    /// assert_eq!(gstr.to_string(), "".to_string());
     /// ```
     pub fn new_if_exists(in_string: &String) -> GlobalString {
-        let map = GLOBAL_STRING_SET.lock().unwrap();
-        let exists: Option<&String> = map.get(in_string);
+        let maps = GLOBAL_STRING_MAP.lock().unwrap();
+        let exists: Option<&u32> = maps.map.get(in_string);
         if exists.is_none() {
             return GlobalString::default();
         }
         return GlobalString {
-            internal_string: exists.unwrap() as *const String
+            string_id: exists.unwrap().clone()
         };  
     }
 
-    /// Gets an immutable reference to the String that the pointer held by GlobalString points to.
+    /// Gets an copy to the String of the id held by GlobalString.
     /// ```
     /// use immie2d_shared::engine_types::global_string::GlobalString;
     /// let gstr = GlobalString::new(&"hello world!".to_string());
-    /// let ref_str: &String = gstr.to_string();
-    /// # assert_eq!(ref_str, &"hello world!".to_string());
+    /// let ref_str: String = gstr.to_string();
+    /// # assert_eq!(ref_str, "hello world!".to_string());
     /// ```
-    pub fn to_string(&self) -> &String {
-        unsafe {
-            &*self.internal_string
-        }
+    pub fn to_string(&self) -> String {
+        let maps = GLOBAL_STRING_MAP.lock().unwrap();
+        let as_string = &maps.vec[self.string_id as usize];
+        return as_string.clone();
     }
 }
 
 impl fmt::Debug for GlobalString {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("GlobalString").field("internal_string",  unsafe { &*self.internal_string}).finish()
+        f.debug_struct("GlobalString").field("internal_string",  &self.to_string()).finish()
     }
 }
 
 impl fmt::Display for GlobalString {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        return write!(f, "{:?}", unsafe { &*self.internal_string});
+        return write!(f, "{:?}", self.to_string());
     }
 }
 
